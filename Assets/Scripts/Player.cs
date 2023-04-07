0c991b02f28896f43d2d0dfe55d76b4eab54d5ea
vgarces <victorgarcesgg@gmail.com>
2023-04-07 12:12:51 -0400

    Add input system
    - Refactor old input manager
    - Add gamepad support

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,31 +4,13 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private float moveRotation = 10f;
+    [SerializeField] private GameInput gameInput;
 
     private bool isWalking;
 
     private void Update()
     {
-        var inputVector = new Vector2(0, 0);
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = +1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = +1;
-        }
-
-        inputVector = inputVector.normalized;
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         var moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
 
         isWalking = moveDir != Vector3.zero;
