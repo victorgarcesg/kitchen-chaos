3b611eba7d4a59ac364ed4fab034501ee3e2d6ee
vgarces <victorgarcesgg@gmail.com>
2023-04-15 23:25:50 -0400

    Add trash counter

---
+++ Assets/Scripts/Counters/TrashCounter.cs
@@ -0,0 +1,10 @@
+public class TrashCounter : BaseCounter
+{
+    public override void Interact(Player player)
+    {
+        if (player.HasKitchenObject())
+        {
+            player.GetKitchenObject().DestroySelf();
+        }
+    }
+}
